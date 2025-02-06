2274c1b9041070b4266a0da6f6c53fbfe266cd7d
Kelvinlins <kelvinlinssilva@hotmail.com>
2025-02-05 21:24:27 -0300

    adicionando policy

--- main.tf
+++ main.tf
@@ -22,3 +22,24 @@ resource "aws_s3_bucket_notification" "evento_s3" {
 output "bucket_name" {
   value = aws_s3_bucket.bucket.id
 }
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = "s3:PutObject"
+        Principal = "*"
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_cloudwatch_event_rule.minha_regra.arn
+          }
+        }
+      }
+    ]
+  })
+}
